049a8a11634d150e86406224bf8f82c47b02a4af
Siphesihle Sehume <siphosihlesehume@gmail.com>
2026-09-06 16:04:43 +0200

    ADD: second inheritance contract

--- contract.sol
+++ contract.sol
@@ -21,6 +21,29 @@ contract Child is Parent {
     function sayHelloFromChild() public pure returns (string memory) {
         return "Hello, from child!";
     }
-
 }
+
+// Second inheritance type
+
+contract Parent_f {
+    function parentFunction() public pure returns (string memory) {
+        return "Called from another contarct";
+    }
+}
+
+contract Child_f {
+    Parent_f externalContract;
+
+    constructor(address _externalContractAddress) {
+        externalContract = Parent_f(_externalContractAddress);
+    }
+
+    function callExternalFunction() public view returns (string memory) {
+        return externalContract.parentFunction();
+    }
+}
+
+
+
+
 
